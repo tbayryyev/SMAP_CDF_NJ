38ad68ba1963fcad036f13903f935313a64cce91
Tahyr Bayryyev <100490172+tbayryyev@users.noreply.github.com>
2022-07-19 10:26:48 -0400

    Update CDF.js

--- CDF.js
+++ CDF.js
@@ -1,26 +1,16 @@
 var NJ = ee.FeatureCollection('users/tahyrb/State_Boundary_of_NJ'); // gets the boundary of NJ
-
 var counties = ee.FeatureCollection('users/tahyrb/County_Boundaries_of_NJ') // gets the counties of NJ
-
 Map.addLayer(counties,{color:'black'},'counties'); // add the counties layer to the map for visulization
-
 Map.setOptions('HYBRID'); // set map to Satellite
-
 
 var dataset = ee.ImageCollection('NASA_USDA/HSL/SMAP10KM_soil_moisture')
                   .filter(ee.Filter.date('2015-04-02', ee.Date(new Date().getTime()))); //filters the date from first observation to latest observation
-               
+           
 var soilMoisture1 = dataset.select('ssm');
-
-
 var dataset_list = soilMoisture1.toList(soilMoisture1.size());   // convert the image collection to the list
-
 var latestIMG = dataset_list.get(dataset_list.size().subtract(1));  // gets the lastest image in the image collection
-
 var dataset_list1 = dataset_list.remove(latestIMG);   // removes the latest image from the image collection
-
 var soilMoisture = ee.ImageCollection(dataset_list1);   // convert the list back to an image collection without the latest image
-
 
 var soilMoistureVis = {
   min: 0.0,
@@ -30,20 +20,14 @@ var soilMoistureVis = {
     
 };
 
-
 var soilMoistureNJ = soilMoisture.map(function(image) { return image.clip(NJ); }); // goes through the image collection and clips each image around NJ boundary
 
 Map.addLayer(soilMoistureNJ, soilMoistureVis, 'Soil Moisture NJ'); // adds SMAP layer to map
-
-
 Map.setCenter(-74.724167,40.072778, 8)  //adds center to NJ
-
 
 ///////////////////////////////////////////Drawing tools////////////////////////////////////
 
-
 var drawingTools = Map.drawingTools();
-
 drawingTools.setShown(false);
 
 while (drawingTools.layers().length() > 0) {
@@ -51,12 +35,10 @@ while (drawingTools.layers().length() > 0) {
   drawingTools.layers().remove(layer);
 }
 
-
 var dummyGeometry =
     ui.Map.GeometryLayer({geometries: null, name: 'geometry', color: '23cba7'});
 
 drawingTools.layers().add(dummyGeometry);
-
 
 function clearGeometry() {
   var layers = drawingTools.layers();
@@ -94,7 +76,6 @@ var symbol = {
   point: '📍',
 };
 
-
 var controlPanel = ui.Panel({
   widgets: [
     ui.Label('1. Select a drawing mode.'),
@@ -125,9 +106,6 @@ var controlPanel = ui.Panel({
 
 
 Map.add(controlPanel);
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 
 // panel for giving info about drought to user 
 // add a panel to give the user some information
@@ -161,30 +139,20 @@ function cdf() {
   var scale = mapScale > 5000 ? mapScale * 2 : 5000;
   
   
-
-   //////////////////////////////////////Creating the list of soil moisture values//////////////////////////
+//////////////////////////////////////Creating the list of soil moisture values//////////////////////////
 
   var sample1 = soilMoistureNJ.getRegion(region,10000);   // reduces the image collection to only images from the chosen point
   var sample = sample1.remove(sample1.get(0));    // remove the first element since it is not a soil moisture value
-
-
-
   var first = [];  // create an empty list for List.iterate function to work 
 
-
-
-// this function takes only the soil moisture value from the list (NOT id, longitude, latitude, etc....)
-
+  // this function takes only the soil moisture value from the list (NOT id, longitude, latitude, etc....)
   var values = function(current,previous){
     var currentQuanitity = ee.List(current).get(4);
     return ee.List(previous).add(ee.Number(currentQuanitity).round());   // values are rounded up
-  
   }
 
 // iterate over the sample image collection to only extract the soil moisture value
   var soilMoisture_values = ee.List(sample.iterate(values,first)).sort();
-
-
 
     ///////////////////////////////////////////////////Creating the list of frequencies //////////////////////////////////////
   
@@ -197,7 +165,6 @@ function cdf() {
     var currentQuantity = soilMoisture_values.filter(ee.Filter.eq('item', ee.Number(current))).size();
     return ee.List(previous).add(currentQuantity);
   }
-  
   
   // iterate over the sm_values list to create a new list of frequencies
   var frequencies = ee.List(ee.List(sm_values).iterate(freqs,first));
@@ -223,39 +190,26 @@ function cdf() {
         probability: ee.List(list).get(1)});
   }));
 
-
-
-
-
 ////////////////////////////////////////////////////////// Giving info to the user on the Map about CDF stats///////////////////////////////
-
 
 // Create a feature collection based on the point geometries drawn.
 var points = ee.FeatureCollection(region);
 
-
-
 // Extract the data from the latest SMAP image at the specified pixel 
 var data1 = ee.Image(latestIMG)
 .reduceRegion(ee.Reducer.first(),region,10).get('ssm');
-
-
+  
 // turn the data into a number value and round
 var value = ee.Number(data1).round();
-
 
 // get the probabilty of this value 
 var probability = cumulativeProbabilities.toList().get(value)
 
-
 // label for showing latest ssm
 var latest_SM = ui.Label("Latest SSM value at this location: "+value.getInfo()+ "mm^3/mm^3");
 
-
 //label for showing prob of latest ssm
 var prob = ui.Label("Probability for this SSM Value at this location based on CDF: " + (ee.Number(probability).format('%.3f')).getInfo());
-
-
 
 // check the 40th percentile of ssm values at this current location and print appropriate msg about flash drought
 if(ee.Number(probability).getInfo()>0.4){
@@ -279,20 +233,13 @@ if(ee.Number(probability).getInfo()>0.4){
 
   
 }
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
 
 // add all the labels we made earlier to the panel on the map for the user
 panel.add(latest_SM);
 panel.add(prob);
 panel.add(msg);
 
-
-
-//////////////////////////////////////////////////////////// displaying the cdf chart 
+//////////////////////////////////////////////////////////// displaying the cdf chart //////////////
   var options = {
     title: 'CDF of Soil Moisture ',
     fontSize: 20,
@@ -309,8 +256,6 @@ panel.add(msg);
     yProperties: ['probability']
   }).setOptions(options);
 
-
-
   // Replace the existing chart in the chart panel with the new chart.
   chartPanel.widgets().reset([cdfChart]);
   
@@ -318,8 +263,6 @@ panel.add(msg);
   panel.widgets().reset([latest_SM,prob,msg]);
   
 }
-
-
 
 drawingTools.onDraw(ui.util.debounce(cdf, 500));
 drawingTools.onEdit(ui.util.debounce(cdf, 500));
